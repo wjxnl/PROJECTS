f7f389bd3cef774f3a3adef67d504c99724f94ab
wjxnl <079bei008@ioepc.edu.np>
2024-12-16 22:41:33 +0545

    updated

--- scissor-paper-rock/app.js
+++ scissor-paper-rock/app.js
@@ -1,2 +1,11 @@
 let userScore = 0;
 let compScore = 0;
+
+const choices = document.querySelectorAll(".image");
+
+choices.forEach((image) =>{
+    image.addEventListener("click", () =>{
+        const choiceId = image.getAttribute("id");
+        console.log("choice was clicked", choiceId);
+    });
+});
